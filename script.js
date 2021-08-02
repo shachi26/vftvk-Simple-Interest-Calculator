9d3d9c2eae1946c1d9f8d3c6e39cdf06a1986528
Shachi Agarwal <shachiagarwal26101@gmail.com>
2021-08-02 13:19:48 +0000

    changes made from theia lab environment

--- script.js
+++ script.js
@@ -1,6 +1,23 @@
-function compute()
-{
-    p = document.getElementById("principal").value;
-    
+function compute() {
+    var p = document.getElementById("principal").value;
+    var r = document.getElementById("rate").value;
+    var t = document.getElementById("years").value;
+    if (p == "" || p <= 0) {
+        alert("Please enter positive value");
+        document.getElementById("principal").focus();
+        return false;
+    }
+
+    var result = (p * r * t) / 100
+    document.getElementById("result").value = result = "If you deposit " + "<span class='highlight'>" + p + "</span>" + "<br>at an interest rate of " + "<span class='highlight'>"
+    r + "</span>%" + "<br>You will receive an amount of " + "<span class='highlight'>" + result + "</span>" +
+        ",<br>in the year " + "<span class='highlight'>" + (2020 + t) + "</span>";
+
 }
-        
+
+function slidervalue() {
+    var slider = document.getElementById("rate");
+    var output = document.getElementById("demo");
+    output.innerHTML = slider.value;
+    slider.oninput = function() { output.innerHTML = this.value; }
+}
